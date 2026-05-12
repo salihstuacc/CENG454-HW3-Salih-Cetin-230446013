99bc5ce4f454b86766e141730eaaa18c07af2806
Salih Cetin <s230446013@stu.thk.edu.tr>
2026-05-12 10:50:53 +0300

    add mouse left click to place towers on the ground

---
+++ Assets/Scripts/PlacementManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlacementManager : MonoBehaviour
+{
+    [Header("Settings")]
+    public GameObject towerPrefab;
+    public LayerMask placementMask;
+
+    [Header("Tower Connections (Kulelere verilecek hedefler)")]
+    public Transform enemyTarget;
+    public ProjectilePool bulletPool;
+    
+    void Update()
+    {
+        if (Mouse.current != null && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            PlaceTower();
+        }
+    }
+    void PlaceTower()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100f, placementMask))
+        {
+            GameObject newTowerObj = Instantiate(towerPrefab, hit.point, Quaternion.identity);
+            Tower newTower = newTowerObj.GetComponent<Tower>();
+            if (newTower != null)
+            {
+                newTower.target = enemyTarget;
+                newTower.pool = bulletPool;
+            }
+            
+            Debug.Log("Sisteme yeni bir kule eklendi ve bağlantıları yapıldı!");
+        }
+    }
+}
